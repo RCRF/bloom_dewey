efa98a42f4bf00a0a43c08c1eb7b7f56f66eeaba
John Major <daylily@daylilyinformatics.com>
2024-06-07 07:00:00 -0700

    v0.8.32

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bloom_lims",
-    version="0.8.31",
+    version="0.8.32",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here,
