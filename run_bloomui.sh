bfe5cf9c5e2b5f6535a7e8c8e5befa9d6d530a77
JEMAJOR@ ubuntu 22.04 <iamh2o@gmail.com>
2024-07-11 23:00:16 +0000

    allow for hosts to be specified

--- run_bloomui.sh
+++ run_bloomui.sh
@@ -1,1 +1,11 @@
-uvicorn main:app --reload --log-level trace --port 8911 --timeout-keep-alive 303
+#source this
+
+# Check if $1 is null and set host accordingly
+if [ -z "$1" ]; then
+  host="0.0.0.0"
+else
+  host="$1"
+fi
+
+# Run uvicorn with the specified host
+uvicorn main:app --reload --log-level trace --port 8911 --timeout-keep-alive 303 --host $host
